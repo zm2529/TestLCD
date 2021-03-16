22fa8283203fb18191cbee621e6464f13368eedc
zm <jiannan@6.cn>
2021-03-16 13:58:05 +0800

    Initial Commit

---
+++ TestLCD/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestLCD
+//
+//  Created by zm on 2021/3/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
